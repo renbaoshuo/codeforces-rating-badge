b268c6824488fc70643ebc36a3a194b4f583aaba
Ren Baoshuo <i@baoshuo.ren>
2020-12-27 09:41:36 +0800

    v0.1

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Codeforces Rating Badge
+
+## Demo
+
+![](https://cfrating.rbs.workers.dev/?username=renbaoshuo)
+
+## Deploy
+
+Login into Cloudflare Dashboard and enter `Workers` App. Create a new script, delete default code in the editor, and then copy [the `woker.js` content](https://github.com/renbaoshuo/codeforces-rating-badge/blob/master/worker.js) into the editor.
+
+Optional: After saving the workers script, you can register a route for the scripts.
+
+## Usage
+
+Just insert the link into your website as an image, like this: `<img src="https://cfrating.rbs.workers.dev/?username=renbaoshuo">`
+
+Do not forget to replace the default configuration with your own!
+
+## Advanced
+
+If you want to restrict your workers only for your website, all you need to do is to edit a few lines of your workers:
+
+- Launch Cloudflare Workers Editor again.
+- You can see some commented out code at the first line like this:
+
+```javascript
+// const AllowedReferrer = 'baoshuo.ren';
+```
+
+- replace your domain with `baoshuo.ren`, then remove `//`.
+
+> **Notice**: set `AllowedReferrer` value to `baoshuo.ren` means all the subdomains of `baoshuo.ren` will be allowed as well.
+
+## Author
+
+**Codeforces Rating Badge** © [Baoshuo](https://github.com/renbaoshuo), Released under the [MIT](https://github.com/renbaoshuo/codeforces-rating-badge/blob/master/LICENSE) License.<br>
+Authored and maintained by Baoshuo with help from contributors ([list](https://github.com/renbaoshuo/codeforces-rating-badge/graphs/contributors)).
+
+> [Personal Website](https://baoshuo.ren) · [Blog](https://blog.baoshuo.ren) · GitHub [@renbaoshuo](https://github.com/renbaoshuo) · Twitter [@renbaoshuo](https://twitter.com/renbaoshuo)
---
+++ worker.js
@@ -0,0 +1,60 @@
+// const AllowedReferrer = 'baoshuo.ren'; // ['baoshuo.ren', 'baoshuo.blog'] multiple domains is supported in array format
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Get badge color
+ * @param {int} rating 
+ */
+function getBadgeColor(rating) {
+    if (rating >= 2400) return "ff0000";
+    if (rating >= 2000) return "ff8c00";
+    if (rating >= 1800) return "aa00aa";
+    if (rating >= 1600) return "0000ff";
+    if (rating >= 1400) return "03a89e";
+    if (rating >= 1200) return "008000";
+    if (rating >= 1000) return "88cc22";
+    return "808080";
+}
+
+/**
+ * Respond to the request
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+    let needBlock = false;
+    if (typeof AllowedReferrer !== 'undefined' && AllowedReferrer !== null && AllowedReferrer) {
+        let _AllowedReferrer = AllowedReferrer;
+        if (!Array.isArray(AllowedReferrer)) _AllowedReferrer = [_AllowedReferrer];
+        const rAllowedReferrer = new RegExp(_AllowedReferrer.join('|'), 'g');
+        needBlock = (!rAllowedReferrer.test(ref_host)) ? true : false;
+        console.log(_AllowedReferrer, rAllowedReferrer, ref_host);
+    }
+    if (needBlock) {
+        return new Response('403 Forbidden', {
+            headers: { 'Content-Type': 'text/html' },
+            status: 403,
+            statusText: 'Forbidden'
+        });
+    }
+    const { searchParams } = new URL(request.url);
+    let username = searchParams.get('username');
+    if (username !== null) {
+        const response = await fetch(`https://codeforces.com/api/user.info?handles=${username}`, { method: 'GET' });
+        const data = await response.json();
+        return new Response('', {
+            status: 302,
+            headers: {
+                location: `https://img.shields.io/badge/${username}-${data.result[0].rank}%20%20${data.result[0].rating}-${getBadgeColor(data.result[0].rating)}.svg?longCache=true&style=for-the-badge&logo=Codeforces&link=https://codeforces.com/profile/${username}`
+            }
+        });
+    }
+    return new Response('', {
+        status: 302,
+        headers: {
+            location: `https://github.com/renbaoshuo/codeforces-rating-badge`
+        }
+    });
+}
